4b30b0f78d820b96ba75fe0990a0d0317c20cdc1
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-04-06 10:42:07 +1000

    Add etch-a-sketch grid and basic functionality
    Prompt user for grid size, add grid, allow either click or mouseover
    events to change tile colour.

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let gridLength
+do {
+  gridLength = parseInt(prompt("Enter the grid size, N, for an N x N grid (maximum 100, otherwise defaults to 16): ")) || 16
+} while (gridLength > 100)
+
+const allowToggle = false // allows infinite toggle of div colour when clicking or hovering over div
+// const eventType = 'mousedown' // individual clicks
+// const eventType = 'click' // individual click
+const eventType = 'mouseover' || 'mousemove' // leaves a trail
+
+
+const container = document.querySelector('#container')
+
+for (let i=0; i<gridLength; i++){
+  const divRow = document.createElement('div')
+  divRow.style.display = 'flex'
+  for (let j=0; j<gridLength; j++){
+    const divColumn = document.createElement('div')
+    divColumn.style.flex = '1 1 auto'
+    divColumn.classList.add('tile')
+    divColumn.style.aspectRatio = '1 / 1'
+    divRow.appendChild(divColumn)    
+  }
+  container.appendChild(divRow)
+}
+
+const divs = document.querySelectorAll('#container > div > div')
+divs.forEach(div => {
+  div.addEventListener(eventType, event => {
+    if (allowToggle){
+      div.classList.toggle('trail')
+    } else {
+      div.classList.add('trail')
+    }
+    event.stopPropagation()
+  })
+})
