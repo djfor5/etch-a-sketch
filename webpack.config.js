ea7388d018acf77e95a918a68d77e2d6e8f9dd49
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-08-25 13:17:44 +1000

    Add more image file extensions to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp|ico)$/i,
         type: "asset/resource",
       },
       {
